81b9771b473aa8c377ed00101c376fafad6ed02e
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-09-20 17:31:22 -0400

    .

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,10 +1,20 @@
 import React, { Component, PropTypes } from 'react'
+import Masonry from 'react-masonry-component';
+import Podcast from './Podcast.jsx';
+import podcasts from '../data/podcasts';
 
 export default class Left extends Component {
   render() {
     return (
       <div>
         Left Side
+          <Masonry>
+          {podcasts.map((item, i) => (
+            <div key={i}>
+              <Podcast item={item} />
+            </div>
+          ))}
+          </Masonry>
       </div>
     )
   }
---
+++ src/data/podcasts.js
@@ -0,0 +1,12 @@
+const podcasts = [
+  {
+    title: 'test 1',
+    description: 'description 1'
+  },
+  {
+    title: 'test 2',
+    description: 'dsqrfqerfwegwergwerg wergwergw ertgwer tg wretg wrtgwrtgwrtg rwtg'
+  }
+]
+
+export default podcasts
