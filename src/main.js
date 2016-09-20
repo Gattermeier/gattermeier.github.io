4fd847e04dbc7908eab2343d3cc139220c443e43
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-09-20 17:02:50 -0400

    .

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-var Hello = React.createClass({
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
   render: function() {
     return <div>Hello {this.props.name}</div>;
   }
 });
 
 ReactDOM.render(
-  <Hello name="World" />,
+  <App name="World" />,
   document.getElementById('main')
 );
